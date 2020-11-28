bc99f51fe4ba47b616207665319cc22e256759f6
Tomiwa Ogunbamowo <contact@tomiwa.io>
2020-11-28 14:06:51 +0000

    Update

--- services/chatbotMessagingService.js
+++ services/chatbotMessagingService.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: `${__dirname}/../../config.env` });
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 const { resolveInclude } = require('ejs');
 
 function WatsonServiceSetUp() {
