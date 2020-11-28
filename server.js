23649d8cdf94df1a7efe5a927549a51fe55abd7f
Tomiwa Ogunbamowo <contact@tomiwa.io>
2020-11-28 14:28:06 +0000

    Fixed env file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 var express = require('express');
 
 var app = express();
