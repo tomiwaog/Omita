1b56b596fbfeefb786058489c579362892bf614e
Tomiwa Ogunbamowo <contact@tomiwa.io>
2020-11-28 14:51:44 +0000

    Fixed env file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set('view engine', 'ejs');
-path.join(__dirname, '../views');
+path.join(__dirname, './views');
 
 app.use('/assets', express.static(__dirname+'/public'));
 app.get('/', function(req,res){
